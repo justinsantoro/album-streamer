e8e1e5c2e11ff83c24be05b622dce83ce1aeb8b2
Justin Santoro <jzsantoro14@gmail.com>
2021-07-04 20:08:15 -0400

    cleanup  library test

--- library/library_test.go
+++ library/library_test.go
@@ -17,32 +17,31 @@ import (
 
 //songs will be played in lexical order
 
-// on server start the root is parsed to build an in memory cache of the library
+// root is parsed to build an in memory cache of the library
 
 // the cache will be an in-memory badger db
 
 // key = 0artist/album/song
-// the album name respectively
 
 // 0 prefix is to leave room for using the first byte to denote
 // "tables"
 
-//build the filepath of the song based on the key
-//given the root path
-
 //lib.Artists() returns list of artists
 //Lib.Albums(Artist) returns an artist's albums
-//Lib.Songs(Artist, Album) returns a list of songs in an album
+//Lib.Songs(Album) returns a list of songs in an album
 
 const songPath = "Artist1/Album1/song1.mp3"
 const artPath = "Artist1/Album1/Album1.webp"
 const testDir = "./testdata"
 
-//const itemlen = 2
+var (
+	tArtists = []string{"Artist1", "Artist2"}
+	tAlbums  = []string{"Album1", "Album2"}
+	tSongs   = []string{"song1.mp3", "song2.mp3", "song3.mp3"}
+)
 
 func TestLibrary(t *testing.T) {
 	var fsys fs.FS
-	//TODO: add test FS
 	fsys = os.DirFS(testDir)
 
 	var lib *library.Library
@@ -61,8 +60,14 @@ func TestLibrary(t *testing.T) {
 			t.FailNow()
 		}
 		if len(artists) != 2 {
-			t.Errorf("albums len only: %d", len(artists))
+			t.Errorf("bad albums len : %d", len(artists))
 			t.FailNow()
+		}
+		for i, artist := range artists {
+			if artist.String() != tArtists[i] {
+				t.Errorf("bad artist %s at index %d", artist.String(), i)
+				break
+			}
 		}
 	})
 
@@ -75,8 +80,14 @@ func TestLibrary(t *testing.T) {
 		}
 		fmt.Println(albums)
 		if len(albums) != 2 {
-			t.Errorf("albums len only: %d", len(albums))
+			t.Errorf("bad albums len: %d", len(albums))
 			t.FailNow()
+		}
+		for i, album := range albums {
+			if album.String() != tAlbums[i] {
+				t.Errorf("bad album %s at index %d", album.String(), i)
+				break
+			}
 		}
 	})
 
@@ -95,11 +106,21 @@ func TestLibrary(t *testing.T) {
 			t.Error(err)
 			t.FailNow()
 		}
+
+		if len(songs) != 2 {
+			t.Errorf("bad songs len: %d", len(songs))
+		}
+		for i, song := range songs {
+			if song.String() != tSongs[i] {
+				t.Errorf("bad song %s at index %d", song.String(), i)
+				break
+			}
+		}
 	})
 
 	t.Run("TestGetSongPath", func(t *testing.T) {
 		if path = songs[0].Path(); path != songPath {
-			t.Errorf("library gave bad songPath: %s - Expected: %s", songs[0].Path(), songPath)
+			t.Errorf("library gave bad song path: %s - Expected: %s", songs[0].Path(), songPath)
 		}
 	})
 }
